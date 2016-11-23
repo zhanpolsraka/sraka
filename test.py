8bcef12dd320a95c98d8e566560ec089fdac1d44
Anna Khaitovich <akhaitovich@gmail.com>
2016-11-23 14:00:02 +0100

    Add colors, beautify and edit makefile

--- test.py
+++ test.py
@@ -11,27 +11,52 @@
 #double values could be named as plus_minus_double_int.ifj16
 #Alternatively, you could name a source code for computing say factorial
 #function as factorial.ifj16. If you can't come up with an appropriate name,
-#use other[UNIQUE ID].ifj16. Have fun with debugging!
-import sys, os, re
+#use other[UNIQUE ID].ifj16. 
+#It's recommended to use Python 2.7. Have fun with debugging!
+import sys, os, subprocess, re
 
-ALL=[f for f in os.listdir('./tests')]
+ALL=[f for f in os.listdir('./tests') if f != 'logs']
 REGEXLIST=[]
 TESTSLIST=[]
 
-if sys.argv[1:] == ['all']:
-	REGEXLIST=ALL
-else:
-	REGEXLIST=sys.argv[1:]
+class colors:
+  OK = '\033[92m'
+  WARNING = '\033[93m'
+  FAIL = '\033[91m'
+  ENDC = '\033[0m'
+  BOLD = '\033[1m'
+  
+def ok(test):
+	print colors.BOLD + test + colors.OK + ' - OK' + colors.ENDC
 
-#Creating TESTLIST
-for r in REGEXLIST:
-	for f in ALL:
-		res = re.search(r, f)
-		if res:
-			TESTSLIST.append(f)
+def notOK(test, expected, got):
+	print colors.BOLD + test + colors.FAIL + ' - FAIL: ' + \
+	'Expected ' + expected + ', but got', got, colors.ENDC
 
-#Executing
-for test in TESTSLIST:
-	os.environ["IFJ16"] = "tests/"+test
-	#print os.environ["IFJ16"]
-	os.system('./ifj16 '+os.environ["IFJ16"])
+def createTestList():
+	if sys.argv[1:] == ['all']:
+		REGEXLIST=ALL
+	else:
+		REGEXLIST=sys.argv[1:]
+	#Creating TESTLIST
+	for r in REGEXLIST:
+		for f in ALL:
+			res = re.search(r, f)
+			if res:
+				TESTSLIST.append(f)
+
+def main():
+	createTestList();
+	for test in TESTSLIST:
+		source = "tests/"+test
+		os.environ["IFJ16"] = source
+		with open('tests/logs/'+test+'.out', 'w+') as out:
+			ret = subprocess.call(['./ifj16'], stdout=out, stderr=out)
+		with open(source, 'r') as f:
+			expected = f.readline().strip('/\n')
+		if ret == int(expected):
+			ok(test)
+		else:
+			notOK(test, expected, ret)
+
+main()
